f2ef510792811906ff7100992055fa78f056697a
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-03 18:18:52 +0530

    [Vinay]Added: Test Cases for for update and Sync with DB

--- Address_Book_Maven_New/src/test/java/UC13/Address_Book_Maven_New/AddressBookDBTest.java
+++ Address_Book_Maven_New/src/test/java/UC13/Address_Book_Maven_New/AddressBookDBTest.java
@@ -3,16 +3,25 @@ package UC13.Address_Book_Maven_New;
 import static org.junit.Assert.*;
 import java.util.List;
 import org.junit.Test;
-
 import UC13.Address_Book_Maven_New.AddressBookService.IOService;
 
 public class AddressBookDBTest {
 
 //	Test case for loading data From DB	
 	@Test
-	public void givenEmployeePayrollInDB_ShouldMatchEmployeeCount() throws AddressBookException {
+	public void givenAddressBookDBShouldReturn_CountOfEntries() throws AddressBookException {
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> addressBookData = addressBookService.readAddressBookData(IOService.DB_IO);
 		assertEquals(6, addressBookData.size());
+	}
+	
+//	Test Case for Updating Contact
+	@Test
+	public void givenNewPhoneForContact_ShouldUpdateInDB() throws AddressBookException {
+		AddressBookService addressBookService = new AddressBookService();
+		List<Contact> addressBookData = addressBookService.readAddressBookData(IOService.DB_IO);
+		addressBookService.updateContactPhone("akuvakka", 12345);
+		boolean result = addressBookService.checkAddressBookInSyncWithDB("akuvakka");
+		assertTrue(result);
 	}
 }
