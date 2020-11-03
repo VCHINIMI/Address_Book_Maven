ee0be3ae904438898acb6d0abdd8ad2e7121abc0
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-03 18:19:51 +0530

    [Vinay]Addded: updatePhone method and checkDBInSync mathod to update and check for sync

--- Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookService.java
+++ Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookService.java
@@ -4,17 +4,41 @@ import java.util.List;
 
 public class AddressBookService {
 	public static List<Contact> addressBookList;
-	private AddressBookDBService addressBookDBService ;
+	private AddressBookDBService addressBookDBService;
+
 	public enum IOService {
 		DB_IO
 	}
-	
+
 	public AddressBookService() {
 		this.addressBookDBService = AddressBookDBService.getInstance();
 	}
+
+	@SuppressWarnings("static-access")
 	public List<Contact> readAddressBookData(IOService ioService) throws AddressBookException {
 		if (ioService.equals(IOService.DB_IO))
 			this.addressBookList = addressBookDBService.readData();
 		return this.addressBookList;
+	}
+
+	public void updateContactPhone(String fname, int phone) throws AddressBookException {
+		int result =  addressBookDBService.updateContactData(fname,phone);
+		if(result == 0)
+			return;
+		Contact conatctData = this.getAddressBookData(fname);
+		if(conatctData != null) conatctData.phone_Number = phone;
+	}
+	
+	@SuppressWarnings("static-access")
+	private Contact getAddressBookData(String fname) {
+		return this.addressBookList.stream()
+					.filter(ContactDataItem -> ContactDataItem.f_Name.equals(fname))
+					.findFirst()
+					.orElse(null);
+	}
+	
+	public boolean checkAddressBookInSyncWithDB(String fname) throws AddressBookException {
+		List<Contact> addressBookDataList = addressBookDBService.getAddressBookData(fname);
+		return addressBookDataList.get(0).equals(this.getAddressBookData(fname));
 	}
 }
