2119d108aaa53a1aec7c77161f6bf60a0cf88513
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-03 18:20:11 +0530

    [Vinay]Addded: updatePhone method and checkDBInSync mathod to update and check for sync

--- Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookDBService.java
+++ Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookDBService.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class AddressBookDBService {
 
 	private static AddressBookDBService addressBookDBService;
+	private java.sql.PreparedStatement addressBookDataStatement;
+	private static List<Contact> addressBookList;
 
 //  Private Constructor for singleton Object	
 	private AddressBookDBService() {
@@ -76,4 +78,76 @@ public class AddressBookDBService {
 			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
 		}
 	}
+	
+//	Updating contact data on DB
+	public int updateContactData(String fname, int phone) throws AddressBookException {
+		return this.updateEmployeeDetailsUsingStatement(fname, phone);		
+	}
+
+	private int updateEmployeeDetailsUsingStatement(String fname, int phone) throws AddressBookException {
+		String sql = String.format("update address_book_table_1 set phone = %d where fname = '%s';", phone, fname);
+		try (Connection connection = this.getConnection()) {
+			java.sql.Statement statement = connection.createStatement();
+			return statement.executeUpdate(sql);
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
+		} catch (AddressBookException e) {
+			throw new AddressBookException(e.getMessage(), e.type);
+		}
+	}
+
+	public List<Contact> getAddressBookData(String name) throws AddressBookException {
+		addressBookList = new ArrayList<Contact>();
+		if (this.addressBookDataStatement == null)
+			this.preparedStatementForAddressBook();
+		try {
+			addressBookDataStatement.setString(1, name);
+			ResultSet resultSet = addressBookDataStatement.executeQuery();
+			addressBookList = this.getAddressBookData(resultSet);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return addressBookList;
+	}
+
+	private List<Contact> getAddressBookData(ResultSet resultSet) throws AddressBookException {
+		List<Contact> addressBookList = new ArrayList<>();
+		try {
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String fname = resultSet.getString("fname");
+				String lname = resultSet.getString("lname");
+				String address = resultSet.getString("address");
+				String city = resultSet.getString("city");
+				String state = resultSet.getString("state");
+				int zip = resultSet.getInt("zipcode");
+				int phone = resultSet.getInt("phone");
+				String email = resultSet.getString("email");
+				String bookName = resultSet.getString("bookname");
+				String bookType = resultSet.getString("booktype");
+				addressBookList.add(new Contact(id, fname, lname, address, city, state, zip, phone, email, bookName, bookType));
+			}
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
+		}
+		return addressBookList;
+	}
+
+	private void preparedStatementForAddressBook() throws AddressBookException {
+		try {
+			Connection connection = this.getConnection();
+			String sql = "select ab.id,ab.fname,ab.lname,a.address,a.city,a.state,a.zipcode,"
+						+ "ab.phone,ab.email, bn.bookname , bt.booktype from address_book_table_1 ab"
+						+ " INNER JOIN address_table_1 a ON ab.id = a.contactId"
+						+ " INNER JOIN book_name_table_1 bn ON ab.id = bn.contact_id"
+						+ " INNER JOIN book_type_table_1 bt ON ab.id = bt.contact_id"
+						+ " WHERE ab.fname = ?";
+			addressBookDataStatement = connection.prepareStatement(sql);
+		} catch (AddressBookException e) {
+			throw new AddressBookException(e.getMessage(), e.type);
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
+		}		
+	}
+
 }
