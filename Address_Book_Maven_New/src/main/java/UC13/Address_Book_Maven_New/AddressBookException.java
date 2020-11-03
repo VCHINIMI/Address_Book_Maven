1480443b9691bd7a90e46af5c9bcd963f4f5105f
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-03 12:32:40 +0530

    [Vinay]Added: AddressBookException class for custom Exception

---
+++ Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookException.java
@@ -0,0 +1,22 @@
+package UC13.Address_Book_Maven_New;
+
+public class AddressBookException extends Exception{
+	private static final long serialVersionUID = 5659569326670024185L;
+
+	public enum ExceptionType {
+		CONNECTION_FAULT, PATH_FAULT, DATABASE_NOT_EXIST, SQL_FAULT;
+	}
+
+	ExceptionType type;
+
+	public AddressBookException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+
+	public AddressBookException(String message, ExceptionType type, Throwable cause) {
+		super(message, cause);
+		this.type = type;
+	}
+
+}
