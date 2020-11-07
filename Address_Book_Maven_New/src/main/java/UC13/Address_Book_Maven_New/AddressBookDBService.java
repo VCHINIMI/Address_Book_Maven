cad61a57459a0c9f306792affdb70eb594268bdb
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-07 13:46:39 +0530

    [Vinay]Added: addNewEmployee method

--- Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookDBService.java
+++ Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookDBService.java
@@ -9,6 +9,11 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mysql.jdbc.Statement;
+import com.mysql.jdbc.integration.jboss.ExtendedMysqlExceptionSorter;
+
+import UC13.Address_Book_Maven_New.AddressBookException.ExceptionType;
+
 
 public class AddressBookDBService {
 
@@ -91,7 +96,8 @@ public class AddressBookDBService {
 		String sql = String.format("update address_book_table_1 set phone = %d where fname = '%s';", phone, fname);
 		try (Connection connection = this.getConnection()) {
 			java.sql.Statement statement = connection.createStatement();
-			return statement.executeUpdate(sql);
+			int result = statement.executeUpdate(sql);
+			return result;
 		} catch (SQLException e) {
 			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
 		} catch (AddressBookException e) {
@@ -182,8 +188,9 @@ public class AddressBookDBService {
 				+ " INNER JOIN book_type_table_1 bt ON ab.id = bt.contact_id" + " WHERE a.state = '%s';";
 		String sql2 = String.format(sql, state);
 		return getDataFromDatabaseBySQL(sql2);
-	}
+	}	
 
+//	Add Contact to DB	
 	public Contact addNewContact(String f_Name, String l_Name, String address, String city, String state, int zip,
 			int phone_Number, String email, String bookName, String bookType, LocalDate date) throws AddressBookException {
 		int contactId = -1;
@@ -194,86 +201,84 @@ public class AddressBookDBService {
 			try {
 				connection.setAutoCommit(false);
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
 			}
 		} catch(AddressBookException e) {
-			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
+			throw new AddressBookException(e.getMessage(), e.type);
 		}
 		
-		try(java.sql.Statement statement = connection.createStatement()) {
-			String sql = String.format("INSERT INTO address_book_table_1(fname,lname,phone,email,date_added) VALUES ('%s','%s',%s,'%s','%s')",f_Name,l_Name,phone_Number,email,Date.valueOf(date));
-			int rowAffected = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
-			if(rowAffected == 1) {
+		// insert into address_book_table
+		try(java.sql.Statement statement = connection.createStatement()){
+			String sql = String.format("INSERT INTO address_book_table_1(fname,lname,phone,email,date_added)"
+					  + " VALUES ('%s','%s', %s ,'%s','%s') ;" ,f_Name,l_Name,phone_Number,email,Date.valueOf(date));
+			int rowsAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);
+			if(rowsAffected == 1) {
 				ResultSet resultSet = statement.getGeneratedKeys();
-				if(resultSet.next()) contactId = resultSet.getInt(1);
+				while(resultSet.next())
+					contactId = resultSet.getInt(1);
 			}
-		}  catch (SQLException e) {
+		} catch(SQLException e) {
 			try {
 				connection.rollback();
 				return contact;
+			} catch(SQLException e1) {
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
+			}
+		}
+		
+		//insert into address_table_1;
+		try(java.sql.Statement statement = connection.createStatement()) {
+			String sql = String.format("INSERT INTO address_table_1(contactId, address, city, state, zipcode) values (%s, '%s','%s','%s', %s); ", contactId,address,city,state,zip);
+			statement.executeUpdate(sql);		
+		} catch(SQLException e) {
+			try {
+				connection.rollback();
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
 			}
-			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
+			return contact;
 		}
 		
+		//insert into bookname
 		try(java.sql.Statement statement = connection.createStatement()) {
-			String sql = String.format("INSERT INTO address_table_1 (contactId, address, city, state, zipcode) VALUES (%s,'%s','%s','%s',%s)",contactId,address,city,state,zip);
-			int rowAffected = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
-		}  catch(SQLException e) {
+			String sql = String.format("INSERT INTO book_name_table_1(contact_Id, bookname) values (%s, '%s'); ", contactId,bookName);
+			statement.executeUpdate(sql);		
+		} catch(SQLException e) {
 			try {
 				connection.rollback();
-				return contact;
-			} catch(SQLException e1) {
-				e1.printStackTrace();
+			} catch (SQLException e1) {
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
 			}
-			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
+			return contact;
 		}
 		
+		// insert into booktype
 		try(java.sql.Statement statement = connection.createStatement()) {
-			String sql = String.format("INSERT INTO bookname_table_1 (contactId, bookname) VALUES (%s,'%s')",contactId,bookName);
-			int rowAffected = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
-		}  catch(SQLException e) {
-			try {
-				connection.rollback();
-				return contact;
-			} catch(SQLException e1) {
-				e1.printStackTrace();
-			}
-			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
-		}
-		
-		try(java.sql.Statement statement = connection.createStatement()) {
-			String sql = String.format("INSERT INTO book_type_table_1 (contactId, booktype) VALUES (%s,'%s')",contactId,bookType);
-			int rowAffected = statement.executeUpdate(sql,statement.RETURN_GENERATED_KEYS);
+			String sql = String.format("INSERT INTO book_type_table_1(contact_Id, booktype) values (%s, '%s'); ", contactId,bookType);
+			int rowAffected = statement.executeUpdate(sql, statement.RETURN_GENERATED_KEYS);		
 			if(rowAffected == 1)
-				contact = new Contact(contactId,f_Name,l_Name,address,city,state,zip,phone_Number,email,bookName,bookType,date);
-		}  catch(SQLException e) {
+				contact = new Contact(contactId, f_Name, l_Name, address, city, state, zip, phone_Number, email, bookName, bookType, date);
+		} catch(SQLException e) {
 			try {
 				connection.rollback();
-				return contact;
-			} catch(SQLException e1) {
-				e1.printStackTrace();
+			} catch (SQLException e1) {
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
 			}
-			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.SQL_FAULT);
 		}
 		
+		// final commit 
 		try {
 			connection.commit();
-		}	catch(SQLException e) {
-			e.printStackTrace();
-		}	finally {
-			if(connection!=null) {
-				try {
-					connection.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		} catch (SQLException e) {
+			throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
+		} finally {
+			//Connection close
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.CONNECTION_FAULT);
 			}
-		}
+		}		
 		return contact;
 	}
 }
