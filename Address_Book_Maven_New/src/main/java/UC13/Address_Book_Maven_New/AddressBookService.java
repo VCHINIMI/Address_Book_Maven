71bb3e418452e5a33ae4f592f729a35348813a8a
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-07 22:00:07 +0530

    [Vinay]Added: adding Contact Array to payroll using iterator

--- Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookService.java
+++ Address_Book_Maven_New/src/main/java/UC13/Address_Book_Maven_New/AddressBookService.java
@@ -1,7 +1,11 @@
 package UC13.Address_Book_Maven_New;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class AddressBookService {
 	public static List<Contact> addressBookList;
@@ -23,28 +27,27 @@ public class AddressBookService {
 	}
 
 	public void updateContactPhone(String fname, int phone) throws AddressBookException {
-		int result =  addressBookDBService.updateContactData(fname,phone);
-		if(result == 0)
+		int result = addressBookDBService.updateContactData(fname, phone);
+		if (result == 0)
 			return;
 		Contact conatctData = this.getAddressBookData(fname);
-		if(conatctData != null) conatctData.phone_Number = phone;
+		if (conatctData != null)
+			conatctData.phone_Number = phone;
 	}
-	
+
 	@SuppressWarnings("static-access")
 	private Contact getAddressBookData(String fname) {
-		return this.addressBookList.stream()
-					.filter(ContactDataItem -> ContactDataItem.f_Name.equals(fname))
-					.findFirst()
-					.orElse(null);
+		return this.addressBookList.stream().filter(ContactDataItem -> ContactDataItem.f_Name.equals(fname)).findFirst()
+				.orElse(null);
 	}
-	
+
 	public boolean checkAddressBookInSyncWithDB(String fname) throws AddressBookException {
 		List<Contact> addressBookDataList = addressBookDBService.getAddressBookData(fname);
 		return addressBookDataList.get(0).equals(this.getAddressBookData(fname));
 	}
-	
+
 	public List<Contact> getContactsBetweenDates(LocalDate start, LocalDate end) throws AddressBookException {
-		List<Contact> contactBetweenDateList = addressBookDBService.getContactsBetweenDates(start,end);
+		List<Contact> contactBetweenDateList = addressBookDBService.getContactsBetweenDates(start, end);
 		return contactBetweenDateList;
 	}
 
@@ -58,10 +61,49 @@ public class AddressBookService {
 		return contactInStateList;
 	}
 
-	public void addNewContactToDB(String f_Name, String l_Name, String address, String city, String state, int zip, int phone_Number,
-			String email, String bookName, String bookType, LocalDate date) throws AddressBookException {
-		addressBookList.add(addressBookDBService.addNewContact(f_Name,l_Name,address,city,state,zip,phone_Number,email,bookName,bookType,date));
-	
+	public void addNewContactToDB(String f_Name, String l_Name, String address, String city, String state, int zip,
+			int phone_Number, String email, String bookName, String bookType, LocalDate date)
+			throws AddressBookException {
+		addressBookList.add(addressBookDBService.addNewContact(f_Name, l_Name, address, city, state, zip, phone_Number,
+				email, bookName, bookType, date));
+
 	}
 
+	public void addContactArrayToDB(Contact[] arrayOfContacts) throws AddressBookException {
+		List<Contact> contactsList = Arrays.asList(arrayOfContacts);
+		Map<Integer, Boolean> employeeAdditionStatus = new HashMap<Integer, Boolean>();
+		Iterator<Contact> iterator = contactsList.iterator();
+		while (iterator.hasNext()) {
+			Contact contact = iterator.next();
+			Runnable task = () -> {
+				employeeAdditionStatus.put(contact.hashCode(), false);
+				System.out.println("Employee Being Added : " + Thread.currentThread().getName());
+				try {
+					this.addNewContactToDB(contact.f_Name, contact.l_Name, contact.address, contact.city, contact.state,
+							contact.zip, contact.phone_Number, contact.email, contact.bookName, contact.bookType,
+							contact.date);
+				} catch (AddressBookException e) {
+					System.out.println(e.getMessage() + " in add contact array");
+				}
+				employeeAdditionStatus.put(contact.hashCode(), true);
+				System.out.println("Emp Added" + Thread.currentThread().getName());
+			};
+			Thread thread = new Thread(task, contact.f_Name);
+			thread.start();
+		}
+
+		while (employeeAdditionStatus.containsValue(false)) {
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				throw new AddressBookException(e.getMessage(), AddressBookException.ExceptionType.THREAD_FAULT);
+			}
+
+		}
+
+	}
+
+	public int countEntries() {
+		return addressBookList.size();
+	}
 }
